ba8c672925220de9e91340262b5a4d7df2e0a28b
Ousama Yamine <35897222+yawaweb@users.noreply.github.com>
2021-02-24 20:57:15 +0100

    Update ConnectionBuilder.php

--- src/Client/ConnectionBuilder.php
+++ src/Client/ConnectionBuilder.php
@@ -8,12 +8,14 @@ class ConnectionBuilder
 {
     public $login;
     public $secretKey;
+    public $sandbox;
     public $lang;
 
-    public function __construct($login, $secretKey, $lang = 'fr')
+    public function __construct($login, $secretKey, $sandbox = false, $lang = 'fr')
     {
         $this->login    = $login;
         $this->secretKey = $secretKey;
+        $this->sandbox = $sandbox;
         $this->lang = $lang;
     }
 }
